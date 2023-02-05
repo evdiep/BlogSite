99df6d8e213b21a9caccb35f20a04b13e06e3095
evdiep <evandiep7@gmail.com>
2023-02-05 17:51:42 -0500

    Initialize Angular Project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'testApp'`, () => {
+  it(`should have as title 'BlogSite'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('testApp');
+    expect(app.title).toEqual('BlogSite');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('testApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('BlogSite app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponentComponent } from './test-component/test-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TestComponentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
